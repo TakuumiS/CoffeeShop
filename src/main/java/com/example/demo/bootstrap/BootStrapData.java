b4b94d8bcbfe9295485db7e17ee99125fce23425
Takumi Sakai <tnsakai1996@gmail.com>
2025-08-08 14:54:28 -0700

    Part E - Add parts and products

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,8 +1,10 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
 import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
@@ -27,50 +29,87 @@ public class BootStrapData implements CommandLineRunner {
 
     private final PartRepository partRepository;
     private final ProductRepository productRepository;
-
+    private final InhousePartRepository inhousePartRepository;
     private final OutsourcedPartRepository outsourcedPartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, InhousePartRepository inhousePartRepository,  OutsourcedPartRepository outsourcedPartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
+        this.inhousePartRepository=inhousePartRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
-       /*
-        OutsourcedPart o= new OutsourcedPart();
-        o.setCompanyName("Western Governors University");
-        o.setName("out test");
-        o.setInv(5);
-        o.setPrice(20.0);
-        o.setId(100L);
-        outsourcedPartRepository.save(o);
-        OutsourcedPart thePart=null;
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            if(part.getName().equals("out test"))thePart=part;
+        if(inhousePartRepository.count()==0) {
+
+            InhousePart mochi = new InhousePart();
+            mochi.setId(1);
+            mochi.setName("Homemade Mochi");
+            mochi.setPrice(5);
+            mochi.setInv(20);
+            inhousePartRepository.save(mochi);
+
+            InhousePart brule = new InhousePart();
+            brule.setId(2);
+            brule.setName("Homemade Brule");
+            brule.setPrice(8);
+            brule.setInv(10);
+            inhousePartRepository.save(brule);
+
+            InhousePart strawberry = new InhousePart();
+            strawberry.setId(3);
+            strawberry.setName("Strawberry puree");
+            strawberry.setPrice(6);
+            strawberry.setInv(10);
+            inhousePartRepository.save(strawberry);
+
         }
 
-        System.out.println(thePart.getCompanyName());
-        */
-        List<OutsourcedPart> outsourcedParts=(List<OutsourcedPart>) outsourcedPartRepository.findAll();
-        for(OutsourcedPart part:outsourcedParts){
-            System.out.println(part.getName()+" "+part.getCompanyName());
+        if(outsourcedPartRepository.count()==0) {
+
+            OutsourcedPart milk = new OutsourcedPart();
+            milk.setCompanyName("Hokkaido Milk Co.");
+            milk.setId(4);
+            milk.setName("Hokkaido Milk");
+            milk.setPrice(10);
+            milk.setInv(10);
+            outsourcedPartRepository.save(milk);
+
+            OutsourcedPart matcha = new OutsourcedPart();
+            matcha.setCompanyName("Matcha Co.");
+            matcha.setId(5);
+            matcha.setName("Matcha");
+            matcha.setPrice(12);
+            matcha.setInv(10);
+            outsourcedPartRepository.save(matcha);
+
         }
 
-        /*
-        Product bicycle= new Product("bicycle",100.0,15);
-        Product unicycle= new Product("unicycle",100.0,15);
-        productRepository.save(bicycle);
-        productRepository.save(unicycle);
-        */
+        if(productRepository.count()==0) {
+            Product latte = new Product("Hokkaido Latte", 10, 25 );
+            Product strawberryLatte = new Product("Strawberry Latte", 12, 30 );
+            Product matchaLatte = new Product("Matcha Latte", 14, 35 );
+            Product mochiLatte = new Product("Mochi Latte", 16, 40 );
+            Product bruleLatte = new Product("Brule Latte", 18, 45 );
+
+            productRepository.save(latte);
+            productRepository.save(strawberryLatte);
+            productRepository.save(matchaLatte);
+            productRepository.save(mochiLatte);
+            productRepository.save(bruleLatte);
+
+
+        }
+
+
+
 
         System.out.println("Started in Bootstrap");
-        System.out.println("Number of Products"+productRepository.count());
+        System.out.println("Number of Products "+productRepository.count());
         System.out.println(productRepository.findAll());
-        System.out.println("Number of Parts"+partRepository.count());
+        System.out.println("Number of Parts "+partRepository.count());
         System.out.println(partRepository.findAll());
 
     }
